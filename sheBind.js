a468b5d3476633f17919b3ab3105e0258f1d70e0
shelizhou <shelizhou88@gmail.com>
2015-09-02 16:43:50 +0800

    初始化
    双向绑定

---
+++ sheBind.js
@@ -0,0 +1,170 @@
+/**
+ * name : sheBind
+ * Description : 数据 双向绑定
+ * createtime: 2014-12-08
+ * version: 0.1
+ * author: she
+ */
+
+;(function(undefined) {
+    
+    "use strict";
+
+    var sheBind = function(obj) {
+        // 返回对象
+        if (!(this instanceof sheBind)) {
+            return new sheBind(obj);
+        }
+
+        var result = {
+                data : obj.data
+            },
+            wrapSel = document.querySelector(obj.el),
+            modelArr = Array.prototype.slice.call(wrapSel.querySelectorAll("[v-model]")),
+            repeatArr = Array.prototype.slice.call(wrapSel.querySelectorAll("[v-repeat]")),
+            nowInputDom = null,
+            setRepeat = function(s) {
+                var name = s["_name"],
+                    tempHtml = s["_tempHtml"],
+                    html = "";
+                obj.data[name].forEach(function(v){
+                    var k = {v : v};
+                    html += tempHtml.replace(/\{(.+)\}/g, function(all, $1){
+                        var mi = $1, vi = k;
+                        mi.split(".").forEach(function(m){
+                            vi = vi[m];
+                        })
+                        return vi || "";
+                    });
+                });
+                s.innerHTML = html;
+            },
+            setModel = function(s, val) {
+                if (s === nowInputDom) {
+                    nowInputDom = null;
+                    return;
+                }
+                if (s.nodeName === "INPUT") {
+                    if ( s.type === "text" ) {
+                        s.value = val;
+                    } else if ( s.type === "radio") {
+                        if (s.value === val) {
+                            s.setAttribute("checked", "checked");
+                        } else {
+                            s.removeAttribute("checked");
+                        }
+                    } else if ( s.type === "checkbox" ) {
+                        if (val.split(",").some(function(v){ return v === s.value; })) {
+                            s.setAttribute("checked", "checked");
+                        } else {
+                            s.removeAttribute("checked");
+                        }
+                    }
+                } else if (s.nodeName === "SELECT"){
+                    Array.prototype.slice.call(s.childNodes).forEach(function(opt){
+                        if (opt.value === val) {
+                            s.selectedIndex = opt.index;
+                        }
+                    });
+                } else if (s.nodeName === "TEXTAREA"){
+                    s.value = val;
+                } else {
+                    s.innerHTML = val;
+                }
+            },
+            fnChange = function(name, val) {
+                modelArr.forEach(function(s){
+                    if (s["_name"] === name){
+                        setModel(s, val);
+                    }
+                });
+                repeatArr.forEach(function(s){
+                    if (s["_name"] === name){
+                        setRepeat(s);
+                    }
+                });
+            };
+
+        modelArr.forEach(function(s){
+            var name = s.getAttribute("v-model");
+            s["_name"] = name;
+            s["_type"] = "model";
+            setModel(s, obj.data[name]);
+
+            if (s.nodeName === "INPUT") {
+                if ( s.type === "text") {
+                    s.addEventListener("input", function(){
+                        nowInputDom = s;
+                        obj.data[name] = s.value;
+                    });
+                } else if (s.type === "radio") {
+                    s.addEventListener("change", function(){
+                        nowInputDom = s;
+                        obj.data[name] = s.value;
+                    });
+                } else if ( s.type === "checkbox" ) {
+                    s.addEventListener("change", function(){
+                        // console.dir(s)
+                        nowInputDom = s;
+                        if ( s.getAttribute("checked") ) {
+                            s.removeAttribute("checked");
+                            var arr = (obj.data[name] !== "" ? obj.data[name].split(",") : []).filter(function(v){ return v !== s.value; });
+                            obj.data[name] = (arr.length > 0 ) ? arr.join(",") : "";
+                        } else {
+                            s.setAttribute("checked", "checked");
+                            var arr = (obj.data[name] !== "") ? obj.data[name].split(",") : [];
+                            arr.push(s.value);
+                            obj.data[name] = (arr.length > 0 ) ? arr.join(",") : "";
+                        }
+                    });
+                }
+            } else if (s.nodeName === "TEXTAREA"){
+                s.addEventListener("input", function(){
+                    nowInputDom = s;
+                    obj.data[name] = s.value;
+                });
+            } else if (s.nodeName === "SELECT"){
+                s.addEventListener("change", function(){
+                    nowInputDom = s;
+                    obj.data[name] = s.value;
+                });
+            }
+        });
+
+        repeatArr.forEach(function(s){
+            s["_name"] = s.getAttribute("v-repeat");
+            s["_tempHtml"] = s.querySelector("script").innerHTML;
+            s["_type"] = "repeat";
+
+            setRepeat(s);
+
+        });
+
+        Object.observe(obj.data, function(changes) {
+            // console.log(changes);
+            changes.forEach(function(v){
+                fnChange(v.name, v.object[v.name]);
+            });
+        });
+
+
+        return result;
+    };
+
+
+
+    // 检测上下文环境是否为 AMD 或者 CMD   
+    if (typeof define === 'function' && (define.amd || define.cmd)) {
+        define(function() {
+            return sheBind;
+        });
+
+    // 检查上下文是否为 node
+    } else if (typeof module !== 'undefined' && module.exports) {
+        module.exports = sheBind;
+        
+    } else {
+        window.sheBind = sheBind;
+    }
+
+})();
